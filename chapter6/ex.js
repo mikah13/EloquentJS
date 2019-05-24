d45d39856d130ab8886f04ccc11d0340a5f92a92
mikah13 <anhminhhoang13@gmail.com>
2019-05-24 01:58:30 -0700

    added exjs ch6

---
+++ chapter6/ex.js
@@ -0,0 +1,12 @@
+/**
+ * Write a class Vec that represents a vector 
+ * in two-dimensional space. It takes x and y 
+ * parameters (numbers), which it should save 
+ * to properties of the same name.
+ * Give the Vec prototype two methods, 
+ * plus and minus, that take another vector 
+ * as a parameter and return a new vector
+ *  that has the sum or difference of the two 
+ * vectors’ (this and the parameter) x and
+ *  y values.
+ */
